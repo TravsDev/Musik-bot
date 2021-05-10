e84b1d0678cf1b2566cce2dc7c0ba7c7f27c6163
DanskeDevelopers <79965637+DanskeDevelopers@users.noreply.github.com>
2021-05-10 17:45:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    token: 'ODI3OTc1NTEwODM5NjU2NDQ4.YGi2SA.vO8DulBrHgoyYkRCJw1CKosTbxI', // Insæt din token her
+    token: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', // Insæt din token her
     prefix: '!' // Sæt din prefix her
 }
 
